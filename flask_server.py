2d57962aaa84e6fa2f7f3e0c9e20c7a977cf64c2
hallowf <alexandre.cortegaca@gmail.com>
2018-09-05 23:26:07 +0100

    Updated flask_server to only deploy sentry on production

--- flask_server.py
+++ flask_server.py
@@ -4,16 +4,26 @@ from app import app
 from flask_cors import CORS
 from raven.contrib.flask import Sentry
 
+FLASK_ENV = os.environ.get("FLASK_ENV", "production")
 
 ### App parameters
-app.config['SECRET_KEY'] = 'secret!'
+app.config.update(
+    ENV = FLASK_ENV,
+    SECRET_KEY = 'secret!'
+)
+
+
+
+if FLASK_ENV == "development":
+    app.config["TESTING"] = True
+else:
+    sentry = Sentry(app, dsn='https://0fefacd0609d400981c0ddec3fbe806b:47f1ea6778ad45d28e928c3bfb945c67@sentry.io/1264488')
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-sentry = Sentry(app, dsn='https://0fefacd0609d400981c0ddec3fbe806b:47f1ea6778ad45d28e928c3bfb945c67@sentry.io/1264488')
 
 
 # Run app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "2890"))
-    app.run( host="0.0.0.0",port=port, debug=True)
+    app.run( host="0.0.0.0",port=port)
