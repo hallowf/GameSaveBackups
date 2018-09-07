cc2a5bc49ebed435e1668f58c6e45d3c6c672f17
hallowf <alexandre.cortegaca@gmail.com>
2018-09-07 18:38:00 +0100

    fixed typo in game_checker function, added vivifyscrum and slack to travis

--- app/Database/db_utilities.py
+++ app/Database/db_utilities.py
@@ -16,7 +16,7 @@ def simple_pickler(mode, object=False , path="game_paths.pckl"):
 ### the modified versions
 def game_checker(game_obj, game_list, game_paths, synced, path_to_sync):
     if synced == "yes" and path_to_sync is not None:
-        game.sync_path = path_to_sync
+        game_obj.sync_path = path_to_sync
     game_obj.found = True
     game_dict = { game_obj.name: {"game_path": game_obj.path, "game_sync_path": game_obj.sync_path}}
     game_paths.append(game_dict)
