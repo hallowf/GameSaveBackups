056eb774e806698f878bbb973990e4a8b771782e
hallowf <alexandre.cortegaca@gmail.com>
2018-09-09 17:17:33 +0100

    passed links as props to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,10 +11,10 @@ class Navbar extends React.Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
+              {this.props.Home}
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="">Features</a>
+              {this.props.HelpPage}
             </li>
           </ul>
         </div>
