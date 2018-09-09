e4a9588164938170bbb771d52e5e98afc839f04f
hallowf <alexandre.cortegaca@gmail.com>
2018-09-09 23:44:09 +0100

    added the main help page with respective functions for rendering all the new pages

--- client/src/components/HelpPage.js
+++ client/src/components/HelpPage.js
@@ -1,27 +1,93 @@
 import React from 'react'
-import UserGuideIndex from './userguide/UserGuideIndex.js'
+import HelpIndex from './userguide/HelpIndex.js'
+import Features from './userguide/Features.js'
+import Troubleshooting from './userguide/Troubleshooting.js'
+import UserGuide from './userguide/UserGuide.js'
+import HowItWorks from './userguide/HowItWorks.js'
+import DatabaseCards from './userguide/DatabaseCards.js'
+import GameDatabaseFilter from './userguide/GameDatabaseFilter.js'
+
+const baseUrl = window.location.hostname === 'localhost' ?
+  window.location.protocol + '//' + document.domain + ':2890/':
+  window.location.protocol + '//' + document.domain + '/'
 
 
 class HelpPage extends React.Component {
   constructor() {
     super()
     this.handleHelpPage = this.handleHelpPage.bind(this)
+    this.returnToIndex = this.returnToIndex.bind(this)
+    this.fetchAllGames = this.fetchAllGames.bind(this)
+    this.searchFilter = this.searchFilter.bind(this)
     this.state = {
-      page: 'index'
+      page: 'index',
+      games: '',
+      filteredGames: '',
+      userSearch: ''
     }
   }
 
+  fetchAllGames (page) {
+    fetch(baseUrl + 'api/games?all_games=no&user_id=no_id&route_testing=2')
+      .then(res => res.json()
+        .then(games => {
+          games = games['game_list'].map(game =>
+            <DatabaseCards key={game['name']} gameName={game['name']} gamePath={game['path']}/>
+          )
+          this.setState({games})
+        })
+      )
+      .catch(e => {
+        this.setState({error: e})
+      })
+  }
+
+  searchFilter(value) {
+    this.setState({userSearch: value})
+    let filteredGames = this.state.games.filter(
+      (game) => {
+        return game.key.indexOf(this.state.userSearch) !== -1
+    })
+    this.setState({filteredGames: filteredGames})
+  }
+
+  returnToIndex() {
+    this.setState({page: 'index'})
+  }
+
   handleHelpPage(page) {
-    console.log(page)
+    this.setState({page})
   }
 
   render () {
     let page = null
-    if (this.state.page === 'index') {
-      page = <UserGuideIndex handleHelpPage={this.handleHelpPage}/>
+    let gameDatabaseUtilities = null
+    if (this.state.page === 'index' || this.state.page !== '') {
+      page = <HelpIndex handleHelpPage={this.handleHelpPage} fetchAllGames={this.fetchAllGames}/>
     }
+    if (this.state.page === 'features') {
+      page = <Features returnToIndex={this.returnToIndex}/>
+    }
+    if (this.state.page === 'troubleshooting') {
+      page = <Troubleshooting returnToIndex={this.returnToIndex}/>
+    }
+    if (this.state.page === 'userguide') {
+      page = <UserGuide returnToIndex={this.returnToIndex}/>
+    }
+    if (this.state.page === 'howitworks') {
+      page = <HowItWorks returnToIndex={this.returnToIndex}/>
+    }
+    if (this.state.games !== '') {
+      page = <div className='row'>{this.state.games}</div>
+      gameDatabaseUtilities = <GameDatabaseFilter searchFilter={this.searchFilter}/>
+    }
+    if (this.state.userSearch !== '') {
+      page = <div className='row'>{this.state.filteredGames}</div>
+    }
+
     return (
       <div>
+        {gameDatabaseUtilities}
         {page}
       </div>
     )
