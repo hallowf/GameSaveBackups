badd79ec320d80aa4f8ff7f044446a84ec3f15f4
hallowf <alexandre.cortegaca@gmail.com>
2018-09-09 17:16:39 +0100

    Added react-router and react-router dom to dependencies and created routes to main page and help page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Navbar from "./components/Navbar"
 import ControlCards from "./components/ControlCards"
 import Raven from 'raven-js'
+import HelpPage from './components/HelpPage'
+import { HashRouter as Router, Route ,Link } from "react-router-dom"
+import { Switch } from 'react-router-dom'
 
 if (process.env.NODE_ENV !== 'development') {
   Raven.config('https://0fefacd0609d400981c0ddec3fbe806b@sentry.io/1264488').install();
@@ -9,11 +12,26 @@ if (process.env.NODE_ENV !== 'development') {
 
 
 class App extends Component {
+
   render() {
     return (
         <div>
-          <Navbar />
-          <ControlCards />
+
+          <header>
+            <Router>
+              <Navbar Home={<Link to='/'>Home</Link>} HelpPage={<Link to='/Help'>Help</Link>}/>
+            </Router>
+          </header>
+
+          <main>
+            <Router>
+              <Switch>
+                <Route exact path='/' component={ControlCards} />
+                <Route exact path='/Help' component={HelpPage} />
+              </Switch>
+            </Router>
+          </main>
+
         </div>
     )
   }
